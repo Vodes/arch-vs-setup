3f36160fad121a1860015f221126cfb37dc7fc86
Alex H. <48185123+Vodes@users.noreply.github.com>
2024-09-08 01:31:22 +0200

    Update setup-env.sh

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 sudo pacman -Syu --noconfirm
-yay --save --answerdiff None --answerclean None
-yay --noconfirm -S visual-studio-code-bin python-pip vapoursynth-tools-genstubs-git qt5-wayland qt6-wayland github-desktop-bin
+yay --save --answerdiff None --answerclean All
+yay --noconfirm -S visual-studio-code-bin python-pip vapoursynth-tools-genstubs-git qt5-wayland qt6-wayland
+yay --noconfirm -R visual-studio-code-bin-debug
+yay --noconfirm -S github-desktop-bin
+yay --noconfirm -R github-desktop-bin-debug
 
 # Export vscode as app on the host
 distrobox-export -a code -ef "--enable-features=UseOzonePlatform --ozone-platform-hint=auto"
